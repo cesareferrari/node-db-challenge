50e08c386967b461ee4f1ab2a5b3f37c49b89970
Cesare Ferrari <cesare@maryandferrari.com>
2019-08-17 14:41:12 -0400

    Add Resource model

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const server = express();
 const projectRouter = require('./routers/project.js');
+const resourceRouter = require('./routers/resource.js');
 
 server.use(express.json());
 server.use('/api/projects', projectRouter);
+server.use('/api/resources', resourceRouter);
 
 server.get('/', (req, res) => {
   res.status(200).send('<h1>Welcome to the Project Tracker API</h1>');
